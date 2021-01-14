b32e9a24bd4b430961f28183364e788a4e5b089a
Mattia <Mattia@DESKTOP-Q6GM7HT.home-life.hub>
2021-01-14 20:31:30 +0100

    aggiunto javadoc parziale e metodo calcolo e inizializzazione main

--- src/calcolatrice/Calcolatrice.java
+++ src/calcolatrice/Calcolatrice.java
@@ -7,7 +7,12 @@ public class Calcolatrice {
     {
         this.risultato = 0;
     }
-
+    
+    /**
+     * Somma due numeri e conserva il risultato / Svolto da Mattia Mercadante
+     * @param num1 il primo numero da sommare
+     * @param num2 il secondo numero da sommare
+     */
     public void somma(double num1, double num2)
     {
         risultato = num1 + num2;
@@ -32,7 +37,26 @@ public class Calcolatrice {
     {
         //Iori
     }
-
+    
+    /**
+     * Esegue il calcolo in base all'operazione / Svolto da Mattia Mercadante e Marius Zaharia
+     * @param operazione l'operazione da eseguire
+     * @param num1 il primo numero passato dal main
+     * @param num2 il secondo numero passato dal main
+     */
+    public void calcolo(String operazione, double num1, double num2)
+    {
+        switch (operazione.charAt(0))
+        {
+            case '+' -> somma(num1, num2);
+            case '-' -> sottrazione(num1, num2);
+            case '*' -> prodotto(num1, num2);
+            case '/' -> divisione(num1, num2);
+            case '%' -> modulo(num1, num2);
+            default -> throw new IllegalStateException("Unexpected value: " + operazione.charAt(0));
+        }
+    }
+    
     public double getRisultato()
     {
         return risultato;
--- src/calcolatrice/Main.java
+++ src/calcolatrice/Main.java
@@ -1,7 +1,15 @@
 package calcolatrice;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Calcolatrice c = new Calcolatrice();
+        Scanner stringInput = new Scanner(System.in);
+        Scanner doubleInput = new Scanner(System.in);
+        String risposta, operazione;
+        double n1=0, n2=0;
+        
     }
 }
